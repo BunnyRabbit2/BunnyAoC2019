37825e1a38f4fb42b83ce370a3744ec9a15bed7f
Lewis Storey (BunnyRabbit2) <iamlewisstorey@yahoo.com>
2020-02-20 20:58:47 +0000

    Day 12 puzzle 2 done Took a bit to understand what the solution is Took a lot longer to work out I had an int overflow problem

--- DayCode/Day12.cs
+++ DayCode/Day12.cs
@@ -19,7 +19,7 @@ namespace AdventOfCode2019
         List<Moon> moons;
 
         public Day12()
-        {            
+        {
             moons = new List<Moon>();
         }
 
@@ -42,17 +42,88 @@ namespace AdventOfCode2019
             }
         }
 
+        void resetSimulation()
+        {
+            foreach(Moon m in moons)
+            {
+                m.resetMoon();
+            }
+        }
+
+        void simulationStep()
+        {
+            foreach (Moon m1 in moons)
+            {
+                foreach (Moon m2 in moons)
+                {
+                    m1.applyGravity(m2);
+                }
+            }
+
+            foreach (Moon m in moons)
+            {
+                m.moveStep();
+            }
+        }
 
         public void solvePuzzle1()
         {
+            if (!inputsLoaded) return;
+
+            int stepsToRun = 1000;
+
+            resetSimulation();
+
+            for (int i = 0; i < stepsToRun; i++)
+            {
+                simulationStep();
+            }
+
             int result = 0;
+
+            foreach (Moon m in moons)
+            {
+                result += m.getEnergy();
+            }
 
             Console.WriteLine("Day12: Puzzle 1 solution - " + result);
         }
 
         public void solvePuzzle2()
         {
-            int result = 0;
+            if (!inputsLoaded) return;
+
+            resetSimulation();
+
+            int steps = 0;
+            long result = 0;
+            
+            long xCycle, yCycle, zCycle;
+            xCycle = yCycle = zCycle = -1;
+
+            while(true)
+            {
+                simulationStep();
+                steps++;
+
+                if(xCycle == -1 && moons.All(m => m.VX == 0))
+                    xCycle = steps*2;
+
+                if(yCycle == -1 && moons.All(m => m.VY == 0))
+                    yCycle = steps*2;
+
+                if(zCycle == -1 && moons.All(m => m.VZ == 0))
+                    zCycle = steps*2;
+
+                if(xCycle > -1 && yCycle > -1 && zCycle > -1)
+                {
+                    result = AOCHelpers.LCM(xCycle, AOCHelpers.LCM(yCycle,zCycle));
+                    break;
+                }
+
+                if(steps > int.MaxValue)
+                    break;
+            }
 
             Console.WriteLine("Day12: Puzzle 2 solution - " + result);
         }
--- Library/Moon.cs
+++ Library/Moon.cs
@@ -5,29 +5,32 @@ namespace AdventOfCode2019
 {
     class Moon
     {
+        public static int MoonId = 0;
+
+        public int id { get; }
+
+        int sX, sY, sZ;
+
         int pX, pY, pZ;
-        public int PX() { return pX; }
-        public int PY() { return pY; }
-        public int PZ() { return pZ; }
+        public int PX { get{return pX;} }
+        public int PY { get{return pY;} }
+        public int PZ { get{return pZ;} }
         public Vector3 getPosition()
         {
-            return new Vector3(pX,pY,pZ);
+            return new Vector3(pX, pY, pZ);
         }
 
         int vX, vY, vZ;
+        public int VX { get{return vX;} }
+        public int VY { get{return vY;} }
+        public int VZ { get{return vZ;} }
 
         public Moon()
         {
             pX = pY = pZ = 0;
             vX = vY = vZ = 0;
-        }
-
-        public Moon(int xIn, int yIn, int zIn)
-        {
-            pX = xIn;
-            pY = yIn;
-            pZ = zIn;
-            vX = vY = vZ = 0;
+            id = Moon.MoonId;
+            Moon.MoonId++;
         }
 
         public Moon(string moonDataIn)
@@ -36,19 +39,33 @@ namespace AdventOfCode2019
             moonDataIn = moonDataIn.Substring(1, moonDataIn.Length - 2);
             string[] pos = moonDataIn.Split(',');
 
-            foreach(string line in pos)
+            foreach (string line in pos)
             {
                 string[] parts = line.Trim().Split('=');
-                if(parts[0] == "x")
+                if (parts[0] == "x")
                     pX = int.Parse(parts[1]);
-                else if(parts[0] == "y")
+                else if (parts[0] == "y")
                     pY = int.Parse(parts[1]);
-                else if(parts[0] == "z")
+                else if (parts[0] == "z")
                     pZ = int.Parse(parts[1]);
             }
 
+            sX = pX;
+            sY = pY;
+            sZ = pZ;
+
             vX = vY = vZ = 0;
 
+            id = Moon.MoonId;
+            Moon.MoonId++;
+        }
+
+        public void resetMoon()
+        {
+            pX = sX;
+            pY = sY;
+            pZ = sZ;
+            vX = vY = vZ = 0;
         }
 
         public void moveStep()
@@ -57,30 +74,28 @@ namespace AdventOfCode2019
             pY += vY;
             pZ += vZ;
         }
-        
-        public void setVelocity(Vector3 velIn)
+
+        public void applyGravity(Moon moonIn)
         {
-            vX = (int)velIn.X;
-            vY = (int)velIn.Y;
-            vZ = (int)velIn.Z;
+            if (id != moonIn.id)
+            {
+                if (pX < moonIn.PX) vX++;
+                else if (pX > moonIn.PX) vX--;
+
+                if (pY < moonIn.PY) vY++;
+                else if (pY > moonIn.PY) vY--;
+
+                if (pZ < moonIn.PZ) vZ++;
+                else if (pZ > moonIn.PZ) vZ--;
+            }
         }
 
-        public void changeVX(bool posX)
+        public int getEnergy()
         {
-            if(posX) vX += 1;
-            else vX -= 1;
-        }
+            int potK = Math.Abs(pX) + Math.Abs(pY) + Math.Abs(pZ);
+            int kinK = Math.Abs(vX) + Math.Abs(vY) + Math.Abs(vZ);
 
-        public void changeVY(bool posY)
-        {
-            if(posY) vY += 1;
-            else vY -= 1;
-        }
-
-        public void changeVZ(bool posZ)
-        {
-            if(posZ) vZ += 1;
-            else vZ -= 1;
+            return potK * kinK;
         }
     }
 }
