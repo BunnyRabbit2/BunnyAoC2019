4a26e848fb71b7d846942e8d1500599d0552dd9a
Lewis Storey (BunnyRabbit2 <iamlewisstorey@yahoo.com>
2020-03-05 22:30:03 +0000

    Day 16 puzzle 1 implemented Tested with a small array

--- DayCode/Day16.cs
+++ DayCode/Day16.cs
@@ -17,11 +17,13 @@ namespace AdventOfCode2019
 
         List<long> inputs;
         bool inputsLoaded;
+        int[] pattern;
 
         public Day16()
-        {            
+        {
             inputs = new List<long>();
             inputsLoaded = false;
+            pattern = new int[] { 0, 1, 0, -1 };
         }
 
         public void loadInputs(string fileLoc)
@@ -30,7 +32,7 @@ namespace AdventOfCode2019
             {
                 string input = File.ReadAllText(fileLoc);
 
-                long i = 0; 
+                long i = 0;
                 var numbers = input.ToArray().Select(c => long.TryParse(c.ToString(), out i) ? i : -1).ToArray().ToList();
 
                 numbers.RemoveAll(n => n == -1);
@@ -45,20 +47,63 @@ namespace AdventOfCode2019
 
         public void solvePuzzle1()
         {
-            if(!inputsLoaded) return;
+            if (!inputsLoaded) return;
 
             long result = 0;
+
+            List<long> testList = new List<long> { 1, 2, 3, 4, 5, 6, 7, 8 };
+            List<long> restR = FFT(testList, 4);
 
             Console.WriteLine("Day16: Puzzle 1 solution - " + result);
         }
 
         public void solvePuzzle2()
         {
-            if(!inputsLoaded) return;
+            if (!inputsLoaded) return;
 
             long result = 0;
 
             Console.WriteLine("Day16: Puzzle 2 solution - " + result);
+        }
+
+        List<long> FFT(List<long> listIn, int steps)
+        {
+            long[] stepArr = listIn.ToArray();
+            long[] nextArr = new long[stepArr.Length];
+
+            for (int i = 0; i < steps; i++)
+            {
+                for (int n = 0; n < stepArr.Length; n++)
+                {
+                    List<int> patternList = new List<int>();
+                    int currentP = 0;
+                    for (int p = 0; p < stepArr.Length + 1; p=p)
+                    {
+                        for (int p2 = 0; p2 < n+1; p2++)
+                        {
+                            patternList.Add(pattern[currentP]);
+                            p++;
+                        }
+                        if (currentP == 3) currentP = 0;
+                        else currentP++;
+                    }
+                    patternList.RemoveAt(0);
+                    long nextN = 0;
+
+                    for (int p = 0; p < stepArr.Length; p++)
+                    {
+                        nextN += patternList[p] * stepArr[p];
+                    }
+
+                    var NNS = nextN.ToString();
+                    nextN = long.Parse(NNS.Substring(NNS.Length-1,1));
+
+                    nextArr[n] = nextN;
+                }
+                stepArr = (long[])nextArr.Clone();
+            }
+
+            return nextArr.ToList();
         }
     }
 }
